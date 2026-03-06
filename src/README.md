3817204a8540b650a06a8d75742c3c1cbb71b139
danScul <156843534+danScul@users.noreply.github.com>
2026-03-05 22:29:44 -0500

    Least squares and new dataset

---
+++ src/README.md
@@ -0,0 +1,6 @@
+Sources Used: https://www.geeksforgeeks.org/maths/least-square-method/
+https://dataframes.juliadata.org/stable/man/getting_started/
+https://docs.julialang.org/en/v1/base/collections/
+https://docs.julialang.org/en/v1/stdlib/Random/#Generators-(creation-and-seeding)
+
+Utilized ChatGpt to better understand how to translate mathmatical elements and how to translate them to code
--- src/main.jl
+++ src/main.jl
@@ -1,29 +1,33 @@
-using Random, DataFrames, LinearAlgebra, Plots #import neccesary libraries
+using Random, DataFrames, LinearAlgebra, Plots, CSV, Statistics #import neccesary libraries
 
 
+df = CSV.read("data/Loadcell_Dataset.csv", DataFrame)
 
-#Generate random dataset for readings
-Random.seed!(1234) # an arbitrary random seed
-n = 200 # number of data points
-x = rand(n) .* 100 # random x values between 0 and 100
+x = df[!, "Voltage (V)"]# sensor results
 
-# Simulate a linear relationship with some noise
-a = 1.45 #a = slope
-b = .25 #b = intercept
-deviation = 2.0 #standard deviation, will be used to generate random deviation
-y = a .* x .+ b .+ deviation * randn(n) #applying the deviation to the normal output (using elementwise multiplication and addition)
+y = df[!, "Weight (gm)"] #true output
 
-df = DataFrame(col1=x, col2= y) # creates a dataframe object to pair the data of the original sensor reading (x) and the 'true' outputs (y)
+x_mean = mean(x) #mean of sensor results
+y_mean = mean(y) #mean of true outputs
 
-#Functionality Check
+numer = sum((x .- x_mean) .* (y .- y_mean)) #numerator, calculates the error between sensor results and teh mean and the same for the outputs
+denom = sum((x .- x_mean).^2) # denom, squared voltage variance
+slope = numer/denom 
+inter = y_mean - (slope * x_mean) #intercept
+y_pred = slope .* x .+ inter # new prediction
 
-#println(first(df, 5))
-#println(last(df, 5))  
-#description = describe(df)
-#using Plots
-#scatter(df.col1, df.col2,
-#    xlabel="Sensor Reading",
-#    ylabel="Measured Output",
-#    title="Synthetic Sensor Dataset",
-#    legend=false)
-#savefig("C:\\Users\\Owner\\Documents\\CSI2560 Project\\figures\\scatter_plot.png")
+println(first(df, 5))
+println(last(df, 5))  
+description = describe(df)
+scatter(df[!, "Voltage (V)"], df[!, "Weight (gm)"],
+    xlabel="Votlage (V)",
+    ylabel="Weight (gm)",
+    title="Loadcell Dataset",
+    legend=false)
+    x_sorted = sort(df[!, "Voltage (V)"])
+    y_sorted = min_slope[1] .+ min_slope[2] .* x_sorted
+    plot!(x_sorted, y_sorted, color=:red, label="Fitted Line")
+savefig("C:\\Users\\Owner\\Documents\\CSI2560 Project\\figures\\scatter_plot.png")
+
+println("Intercept: ", min_slope[1])
+println("Slope: ", min_slope[2])
